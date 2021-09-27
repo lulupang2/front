4db34c44059260e019dd3576b0ea71c7e656c013
lulupang2 <lulupang@live.co.kr>
2021-09-27 20:23:55 +0900

    -

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+import * as types from './ActionTypes';
+
+export const boardlist = () => ({
+    type: types.INCREMENT
+});
+
--- src/pages/write.jsx
+++ src/pages/write.jsx
@@ -2,25 +2,68 @@ import Editor from "./editor";
 import React from 'react'
 import { Link } from "react-router-dom";
 import { useState } from 'react';
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
-const Write = () => {
+
+const Write = (props) => {
 
     const [id, setId] = useState(0);
     const [title, setTitle] = useState('');
+    const [name, setName] = useState('');
+    const [body, setBody] = useState('');
+    const [date, setDate] = useState('');
     const [tags, setTags] = useState([]);
+
+    const titleHandler = (e) => {
+        setTitle(e.currentTarget.value);
+    }
+
+    const nameHandler = (e) => {
+        setName(e.currentTarget.value);
+    }
+
+    const bodyHandler = (e) => {
+        setBody(e.currentTarget.value);
+    }
+    const tagsHandler = (e) => {
+        if (e.match(/#[^\s]+/)) {
+            setTags(e.currentTarget.value);
+        };
+
+    }
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        const data = {
+            title: title,
+            name: name,
+            body: body,
+            tags: tags
+            
+        }
+        axios.post('http://testest2.cf:4000/board', data)
+            .then(res => console.log(res.data));        
+
+    }
+
+
 
     return (
         <>
-            <form className="w-full px-3 py-3">
+            <form className="w-full px-3 py-3" onSubmit={submitHandler}>
                 <div className="flex items-center border-b border-teal-500 py-2 px-3 my-5">
-                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="제목" aria-label="Title" />
+                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        type="text" placeholder="제목" aria-label="Title" value={title} onChange={titleHandler}/>
                 </div>
                 <div className="w-48 border-b border-teal-500 py-2 px-3 my-5">
-                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="작성자" aria-label="Name" />
+                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        type="text" placeholder="작성자" aria-label="Name" value={name} onChange={nameHandler}/>
                 </div>
-                <Editor />
+                <Editor value={body} onChange={bodyHandler}/>
                 <div className="flex items-center border-b border-teal-500 py-2 px-3">
-                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="태그 입력 쉼표(,)로 구분해주세요" aria-label="tags" />
+                    <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        type="text" placeholder="태그 입력 쉼표(,)로 구분해주세요" aria-label="tags" value={[tags]} onChange={tagsHandler} />
                 </div>
                 <div className="items-center my-1 py-2 px-3">
 
@@ -34,6 +77,7 @@ const Write = () => {
             </form>
 
         </>
-    )}
+    )
+}
 
 export default Write
